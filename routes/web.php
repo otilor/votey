e0172997190cb83807de6406e27f50fbf3827ced
Gabriel Femi <gabrielfemi799@gmail.com>
2020-08-31 06:40:13 +0100

    Add dashboard controller for admins

--- routes/web.php
+++ routes/web.php
@@ -25,17 +25,18 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
+Route::get('upgrade', fn () => auth()->user()->assignRole('admin'));
+
 Route::get('/home', 'HomeController@index')->name('home');
 
-
-Route::group(['prefix' => 'admin'], function () {
-   Route::get('dashboard', fn () => 'Welcome to the admin dashboard!');
+Route::group(['prefix' => 'admin', 'middleware' => 'role:admin'], function () {
+   Route::get('dashboard', 'Admin\DashboardController');
 });
 
-Route::group(['prefix' => 'student'], function () {
+Route::group(['prefix' => 'student', 'middleware' => 'role:student'], function () {
     Route::get('dashboard', fn () => 'Welcome to the student dashboard!');
 });
 
-Route::group(['prefix' => 'contestant'], function () {
+Route::group(['prefix' => 'contestant', 'middleware' => 'role:contestant'], function () {
     Route::get('dashboard', fn () => 'Welcome to the contestant dashboard!');
 });
