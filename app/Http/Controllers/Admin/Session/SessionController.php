05f230d3e699274fa0ad088087714177fb2673e7
Gabriel Femi <gabrielfemi799@gmail.com>
2020-08-31 17:26:13 +0100

    Add request class for session creation

--- app/Http/Controllers/Admin/Session/SessionController.php
+++ app/Http/Controllers/Admin/Session/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Session;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateSessionRequest;
 use Illuminate\Http\Request;
 
 class SessionController extends Controller
@@ -33,7 +34,7 @@ class SessionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateSessionRequest $request)
     {
         //
     }
--- resources/views/admin/sessions/create.blade.php
+++ resources/views/admin/sessions/create.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <form>
+        <form action = "/admin/sessions/store" method="post">
+            @csrf
+            @method("put")
             <div class="form-group">
                 <label for="exampleFormControlInput1">Year</label>
                 <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="2020/2021">
