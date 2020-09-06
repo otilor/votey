03c493d95bdb827d3be201f6aab2588062c03baa
Gabriel Femi <gabrielfemi799@gmail.com>
2020-09-06 13:28:57 +0100

    Change contestant view

--- resources/views/admin/positions/show.blade.php
+++ resources/views/admin/positions/show.blade.php
@@ -5,13 +5,27 @@
         <hr>
 
         <h5 class="text-uppercase">contestants</h5>
-        @forelse($position->contestants as $contestant)
-            <div class="card my-2">
-                <div class="card-header py-2">{{ $contestant->user->name  }}</div>
-            </div>
-        @empty
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Votes</th>
+            </tr>
+            </thead>
+            <tbody>
+            @for($i = 0; $i < count($position->contestants); $i++)
+            <tr>
+                <th scope="row">1</th>
+                <td>{{ $position->contestants[$i]->user->name  }}</td>
+                <td>{{ $position->contestants[$i]->votes  }}</td>
+            </tr>
+            @endfor
+            </tbody>
+        </table>
+        @empty($position->contestants)
             <p>No contestants</p>
-        @endforelse
+        @endempty
         <a class="btn btn-success" href="/admin/polls/{{ $position->poll_id  }}/positions/{{ $position->id  }}/contestants/create">Create</a>
     </div>
 @endsection
