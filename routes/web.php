506dce919a634b94f34483dbd9f3934c99897bc5
Akinyosoye Gabriel <gabrielfemi799@gmail.com>
2020-09-20 19:37:09 +0100

    Apply fixes from StyleCI (#9)

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'role:admin'], function () {
